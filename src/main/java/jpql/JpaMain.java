2673cb6344b4365765dab72aabcaa1030b69f7d8
Kim Juho <sv05159@naver.com>
2021-10-07 00:21:49 +0900

    프로젝션
    프로젝션
    - SELECT 절에 조회할 대상을 지정하는 것
    - 프로젝션 대상: 엔티티, 임베디드 타입, 스칼라 타입
    
    프로젝션 - 여러 값 조회
    1. Query 타입으로 조회
    2. Object[] 타입으로 조회
    3. new 명령어로 조회

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -2,6 +2,8 @@ package jpql;
 
 import javax.persistence.*;
 
+import java.util.List;
+
 import static javax.persistence.Persistence.createEntityManagerFactory;
 
 public class JpaMain {
@@ -20,10 +22,11 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
-            System.out.println("result = " + result.getUsername());
+            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("memberDTO = " + memberDTO.getUsername());
+            System.out.println("memberDTO = " + memberDTO.getAge());
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
